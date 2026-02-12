6bc914db375ba91a764203e9691d14694d310903
Avinash Yeccaluri <avinash.yeccaluri@reodigital.com>
2026-02-12 16:41:21 +0530

    Reversing array

--- Problem Solving/Arrays.js
+++ Problem Solving/Arrays.js
@@ -8,4 +8,17 @@ for (let i = 0; i < a.length; i++) {
     if(a[i] < min) min = a[i];
 }
 
-console.log(min + max) // testing
+console.log(min + max) //
+
+// Given an array A of N integers and also given two integers B and C.
+//     Reverse the elements of the array A within the given inclusive range [B, C].
+
+// Sample Input
+// A = [1, 2, 3, 4]
+// B = 2
+// C = 3
+
+// reverse an array
+
+// Array Rotation
+
